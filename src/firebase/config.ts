53bc68a1ce586bd0c4bd00ddc3bffbd4df6562fb
pastory joseph <pastatrade101@gmail.com>
2025-10-27 06:51:40 +0000

    Set up a Firebase backend

---
+++ src/firebase/config.ts
@@ -0,0 +1,8 @@
+export const firebaseConfig = {
+  "projectId": "studio-4061903538-cceaf",
+  "appId": "1:613590583285:web:c3996b0839adc4a30ac2bc",
+  "apiKey": "AIzaSyDMD56K2on7eofqmbB69TZMyxLtoZZ_9HE",
+  "authDomain": "studio-4061903538-cceaf.firebaseapp.com",
+  "measurementId": "",
+  "messagingSenderId": "613590583285"
+};
