3c21db4bcd413dc2ff6847fc9c763630f2c99408
pastory joseph <pastatrade101@gmail.com>
2025-11-19 10:54:25 +0000

    Try fixing this error: `Runtime Error: Error: Invalid src prop (https://

--- next.config.ts
+++ next.config.ts
@@ -34,6 +34,12 @@ const nextConfig: NextConfig = {
         port: '',
         pathname: '/**',
       },
+      {
+        protocol: 'https',
+        hostname: 'thumbs.dreamstime.com',
+        port: '',
+        pathname: '/**',
+      },
     ],
   },
 };
