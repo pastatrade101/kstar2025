b224e4e0b320f4ea19f28856206a8b6d0976190e
pastory joseph <pastatrade101@gmail.com>
2025-11-20 13:05:31 +0000

    remove those top stats cards

--- src/app/admin/dashboard/page.tsx
+++ src/app/admin/dashboard/page.tsx
@@ -1,20 +1,16 @@
 'use client';
 
-import { StatsCards } from '@/components/admin/StatsCards';
 import { QuickActions } from '@/components/admin/QuickActions';
 import { RecentActivity } from '@/components/admin/RecentActivity';
 
 export default function AdminDashboardPage() {
   return (
-    <div className="space-y-8">
-      <StatsCards />
-      <div className="grid grid-cols-1 lg:grid-cols-3 gap-8">
-        <div className="lg:col-span-2">
-          <QuickActions />
-        </div>
-        <div>
-          <RecentActivity />
-        </div>
+    <div className="grid grid-cols-1 lg:grid-cols-3 gap-8">
+      <div className="lg:col-span-2">
+        <QuickActions />
+      </div>
+      <div>
+        <RecentActivity />
       </div>
     </div>
   );
